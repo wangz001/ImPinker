032678da825400f948797422e9813b37df16877b
wangz001 <bwlang@sina.cn>
2016-06-27 15:46:16 +0800

    controller

---
+++ ImPinker/ImpinkerApi/Controllers/ArticleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImpinkerApi.Controllers
+{
+    public class ArticleController : Controller
+    {
+        //
+        // GET: /Article/
+
+        public string Index()
+        {
+            return string.Empty;
+        }
+
+    }
+}
---
+++ ImPinker/ImpinkerApi/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImpinkerApi.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+
+        public string Index()
+        {
+            return "";
+        }
+
+    }
+}
