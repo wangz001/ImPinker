9b43097d5142cd362d34ae5fd6bfddcabd1964f0
wangz001 <bwlang@sina.cn>
2016-06-29 14:13:53 +0800

    search

---
+++ ImPinker/ImpinkerMobile/Controllers/SearchController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+using Newtonsoft.Json;
+
+namespace ImpinkerMobile.Controllers
+{
+    public class SearchController : Controller
+    {
+        private const int IndexPageCount = 10;
+        //
+        // GET: /Search/
+
+        public ActionResult Index(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                key = "越野";
+            }
+            string result = GetByPage(key, 1, 10);
+            ViewBag.ArticleJson = result;
+            ViewBag.pageCount = IndexPageCount;
+            return View();
+        }
+
+        private string GetByPage(string key, int pageNum, int pageCount)
+        {
+            if (!string.IsNullOrEmpty(key))
+            {
+                List<SolrSearchBll.ArticleViewModel> list = SolrSearchBll.Query(key, pageNum, pageCount);
+                if (list != null && list.Count > 0)
+                {
+                    return JsonConvert.SerializeObject(list);
+                }
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 分页获取数据
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="pageNum">页码</param>
+        /// <param name="pageCount">每页的个数</param>
+        /// <returns></returns>
+        [HttpGet]
+        public string GetNextPage(string key, int pageNum, int pageCount)
+        {
+            return GetByPage(key, pageNum, pageCount);
+        }
+    }
+}
---
+++ ImPinker/ImpinkerMobile/Scripts/Search/search.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+    //设置第一页数据
+    if (jsonData != "") {
+        var data = jsonData;
+        setCard(data);
+    }
+
+    //加载下一页数据
+    $("#loadmore").bind('click', function () {
+        pageNum = pageNum + 1;
+        getNextPage(pageNum, pageCount);
+    });
+    function getNextPage(pageNum, pageCount) {
+        var key = $.getUrlParam("key");
+        $.ajax({
+            url: "/Search/GetNextPage",
+            type: "get",
+            data: { key: key, pageNum: pageNum, pageCount: pageCount },
+            success: function (data) {
+                if (data == null || data == "") {
+                    $("#loadmore").parent().hide();
+                } else {
+                    var list = JSON.parse(data);
+                    setCard(list);
+                }
+            },
+            error: function (data) {
+                alert(data);
+            }
+        });
+    }
+    //通过模板填充数据
+    function setCard(dataJson) {
+        if (dataJson != null && dataJson.length > 0) {
+            //获取模板上的HTML
+            var html = $('script[type="text/template1"]').html();
+            //定义一个数组，用来接收格式化合的数据
+            var arr = [];
+            //对数据进行遍历
+            $.each(dataJson, function (i, o) {
+                //这里取到o就是上面rows数组中的值, formatTemplate是最开始定义的方法.
+                arr.push(html.temp(o));
+            });
+            $('#articleConten').append(arr.join(''));
+        }
+    };
+});
