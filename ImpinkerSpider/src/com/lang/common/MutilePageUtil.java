63461483caff61fc671309b792063ddc61a148a5
wangz001 <bwlang@sina.cn>
2016-09-08 17:27:19 +0800

    fbpagination

--- ImpinkerSpider/src/com/lang/common/MutilePageUtil.java
+++ ImpinkerSpider/src/com/lang/common/MutilePageUtil.java
@@ -8,6 +8,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.lang.fblife.FbLifeXPathCommon;
+import com.lang.util.TUtil;
+
 public class MutilePageUtil {
 
 	private volatile static MutilePageUtil mutilePageUtil = null;
@@ -64,7 +67,64 @@ public class MutilePageUtil {
 				}
 			}
 		});
-
+		CombineFB(pages);
 		System.out.println("合并。。。。。。。。。。");
+	}
+
+	private void CombineFB(List<MutilePageModel> pages) {
+		MutilePageModel model = pages.get(0);
+		String otherContent = "";
+		for (MutilePageModel mutilemodel : pages) {
+			if (pages.indexOf(mutilemodel) == 0) {
+				continue;
+			}
+			otherContent += FbLifeXPathCommon.getContentString(mutilemodel
+					.getPage());
+		}
+		String titleString = FbLifeXPathCommon.getTitleString(model.getPage());
+		if (titleString != null && titleString.length() > 0) {
+			String url = FbLifeXPathCommon.getUrl(model.getPage());
+			String firstImg = FbLifeXPathCommon.getFirstImg(model.getPage());
+			String keyWord = FbLifeXPathCommon
+					.getKeyWordString(model.getPage());
+			String description = FbLifeXPathCommon.getDescription(model
+					.getPage());
+			String content = FbLifeXPathCommon
+					.getContentString(model.getPage()) + otherContent;
+			String publishTime = FbLifeXPathCommon.getPublishTime(model
+					.getPage());
+
+			Article article = new Article();
+			article.setTitle(titleString);
+			article.setKeyWord(keyWord);
+			if (description.length() > 200) {
+				article.setDescription(description.substring(0, 198));
+			} else {
+				article.setDescription(description);
+			}
+			article.setUrlString(url);
+			article.setCompany(CompanyEnum.Fblife.getName());
+			article.setCoverImage(firstImg);
+			article.setContent(content);
+			String timeString = "" != publishTime ? publishTime : TUtil
+					.getCurrentTime();
+
+			article.setCreateTime(timeString);
+			// 根据url获取id。如果id>0，表示存在，则重新做索引。如果==0，则表示不存在
+			ArticleDao articleDao = new ArticleDao();
+			long id = articleDao.GetIdByUrl(article.urlString);
+			if (id > 0) {
+				article.setId(id);
+			} else {
+				id = articleDao.Add(article);
+				article.setId(id);
+			}
+			String timeStr = TUtil.strToUTCTime(article.getCreateTime());
+			article.setCreateTime(timeStr); // 转成utc时间格式
+			// 添加索引
+			SolrJUtil solrJUtil = SolrJUtil.getInstance();
+			solrJUtil.AddDocs(article);
+		}
+
 	}
 }
