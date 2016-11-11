6a07ab5caeed0591f065403e823bf348a5e09db8
wangz001 <bwlang@sina.cn>
2016-11-11 19:10:08 +0800

    mycenter

--- ImPinker/ImPinker/Controllers/UserCenterController.cs
+++ ImPinker/ImPinker/Controllers/UserCenterController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BLL;
+using Microsoft.AspNet.Identity;
+using Newtonsoft.Json;
 
 namespace ImPinker.Controllers
 {
     public class UserCenterController : Controller
     {
-        //
+        private static readonly ArticleBll ArticleBll = new ArticleBll();
+        private static readonly UserBll UserBll = new UserBll();
+        //个人中心首页
         // GET: /UserCenter/
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult MyArticles()
+        {
+            var userId = UserBll.GetModelByAspNetId(User.Identity.GetUserId()).Id;
+            var ds = ArticleBll.GetMyListByPage(userId, 1, 10);
+            var articles = ArticleBll.DataTableToList(ds.Tables[0]);
+            ViewBag.jsonData = JsonConvert.SerializeObject(articles);
+            ViewBag.pageCount = 10;
             return View();
         }
 	}
