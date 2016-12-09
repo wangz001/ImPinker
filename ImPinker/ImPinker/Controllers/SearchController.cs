7147b54812d116731e13b97a3cb6fc17b3f3eb99
wangz001 <bwlang@sina.cn>
2016-12-09 18:55:45 +0800

    hotTag

--- ImPinker/ImPinker/Controllers/SearchController.cs
+++ ImPinker/ImPinker/Controllers/SearchController.cs
@@ -13,7 +13,7 @@ namespace ImPinker.Controllers
         // GET: 搜索，获取首页数据
         public ActionResult Index(SearchDto dto)
         {
-            if (dto.PageNum==0)
+            if (dto.PageNum == 0)
             {
                 dto.PageNum = 1;
             }
@@ -33,7 +33,7 @@ namespace ImPinker.Controllers
         [HttpGet]
         public string GetNextPage(SearchDto dto)
         {
-            var searchvm= GetByPage(dto);
+            var searchvm = GetByPage(dto);
             if (searchvm.ArticleList != null && searchvm.ArticleList.Count > 0)
             {
                 return JsonConvert.SerializeObject(searchvm.ArticleList);
@@ -43,15 +43,37 @@ namespace ImPinker.Controllers
 
         private SearchResultVm GetByPage(SearchDto dto)
         {
-            var searchvm=new SearchResultVm();
-            if (string.IsNullOrEmpty(dto.Key)&&string.IsNullOrEmpty(dto.Tab)) return searchvm;
+            var searchvm = new SearchResultVm();
+            if (string.IsNullOrEmpty(dto.Key) && string.IsNullOrEmpty(dto.Tab)) return searchvm;
             var urlDecode = System.Web.HttpUtility.UrlDecode(dto.Key);
             if (urlDecode != null)
                 dto.Key = urlDecode.Replace(" ", ",");  //url解码，去除特殊字符
 
-            searchvm = SolrNetSearchBll.Query(dto.Key, dto.Tab, dto.FacetCompany, dto.FacetTag,dto.FacetDateTime, dto.PageNum , dto.PageCount);
+            searchvm = SolrNetSearchBll.Query(dto.Key, dto.Tab, dto.FacetCompany, dto.FacetTag, dto.FacetDateTime, dto.PageNum, dto.PageCount);
 
             return searchvm;
+        }
+
+        /// <summary>
+        /// 首页热门标签分类及文章热门标签页面
+        /// </summary>
+        /// <param name="hotTag"></param>
+        /// <returns></returns>
+        public ActionResult HotTag(string hotTag)
+        {
+            var dto = new SearchDto()
+            {
+                FacetCompany = "",
+                FacetDateTime = "",
+                FacetTag = "",
+                Key = hotTag,
+                PageNum = 1,
+                PageCount = 30,
+                Tab = hotTag
+            };
+            var searchvm = GetByPage(dto);
+            ViewBag.searchVm = searchvm;
+            return View();
         }
     }
 }
