96aa55a2151e556f10c9a3cd23a4691407fb25e1
wangz001 <bwlang@sina.cn>
2017-01-12 23:32:49 +0800

    11

---
+++ ImPinker/ImPinker/Controllers/UserRelationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImPinker.Controllers
+{
+    public class UserRelationController : Controller
+    {
+        //
+        // GET: /UserRelation/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
