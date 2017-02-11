c4329652c291a23573995de91071f376b71186f9
wangz001 <bwlang@sina.cn>
2017-02-11 23:27:21 +0800

    hbuilder
    hbuilder 客户端

--- ImPinker/ImpinkerApi/Controllers/ArticleController.cs
+++ ImPinker/ImpinkerApi/Controllers/ArticleController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ImBLL;
+using ImModel.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,8 @@ namespace ImpinkerApi.Controllers
 {
     public class ArticleController : Controller
     {
+
+        private static readonly ArticleBll ArticleBll = new ArticleBll();
         //
         // GET: /Article/
 
@@ -16,5 +20,33 @@ namespace ImpinkerApi.Controllers
             return string.Empty;
         }
 
+        /// <summary>
+        /// 分页获取数据
+        /// </summary>
+        /// <param name="pageNum"></param>
+        /// <param name="pageCount"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult GetByPage(int pageNum, int pageSize)
+        {
+            var list = GetListsByPage(pageNum, pageSize);
+            return Json(new { success = true, data = list },JsonRequestBehavior.AllowGet); 
+        }
+
+        private List<ArticleViewModel> GetListsByPage(int pageNum, int pageCount)
+        {
+            //如果是新用户，则推荐热门文章；老用户，则根据用户兴趣标签，智能推荐
+            var list = new List<ArticleViewModel>();
+            var userInterestKey = "";
+            if (string.IsNullOrEmpty(userInterestKey))
+            {
+                list = ArticleBll.GetIndexListByPage(pageNum, pageCount);
+            }
+            else
+            {
+            }
+            
+            return list;
+        }
     }
 }
