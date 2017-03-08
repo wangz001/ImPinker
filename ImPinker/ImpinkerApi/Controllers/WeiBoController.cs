ff3225c136cf935c1bd3683b93bf040139207444
wangz001 <bwlang@sina.cn>
2017-03-09 00:16:09 +0800

    new

--- ImPinker/ImpinkerApi/Controllers/WeiBoController.cs
+++ ImPinker/ImpinkerApi/Controllers/WeiBoController.cs
@@ -7,6 +7,13 @@ using System.Web.Mvc;
 using ImBLL;
 using ImModel;
 using ImpinkerApi.Models;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
+using ImModel.Enum;
+using Common.AlyOssUtil;
 
 namespace ImpinkerApi.Controllers
 {
@@ -18,6 +25,7 @@ namespace ImpinkerApi.Controllers
         /// </summary>
         /// <param name="vm"></param>
         /// <returns></returns>
+        [HttpPost]
         public HttpResponseMessage Add(NewWeiBoViewModel vm)
         {
             if (vm!=null&&(!string.IsNullOrEmpty(vm.Description)||!string.IsNullOrEmpty(vm.ContentValue)))
@@ -55,5 +63,133 @@ namespace ImpinkerApi.Controllers
                 Description = "error"
             });
         }
+
+        /// <summary>
+        /// mui 多图上传
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<HttpResponseMessage> NewWeibo()
+        {
+            // 检查是否是 multipart/form-data 
+            if (!Request.Content.IsMimeMultipartContent("form-data"))
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new JsonResultViewModel
+                {
+                    IsSuccess = 0,
+                    Description = "数据格式错误",
+                    Data = HttpStatusCode.UnsupportedMediaType
+                });
+            }
+            string fileSaveLocation = HttpContext.Current.Server.MapPath("~/ImageUpload/"+DateTime.Now.ToString("yyyyMMdd"));
+            if (!Directory.Exists(fileSaveLocation))
+            {
+                Directory.CreateDirectory(fileSaveLocation);
+            }
+            var provider = new CustomMultipartFormDataStreamProvider(fileSaveLocation);
+
+            string buckeyName = "myautos";
+            try
+            {
+                var rootPath = HttpContext.Current.Server.MapPath("~/ImageUpload");
+                // Read all contents of multipart message into CustomMultipartFormDataStreamProvider.  
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                //封装数据
+                var weiboVm = InitWeiBoData(provider);
+                List<string> files = new List<string>();
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    string ImgUrlformat = "weiboimage/{0}/{1}_{2}.jpg";
+                    var imgUrl = string.Format(ImgUrlformat, DateTime.Now.ToString("yyyyMMdd"), weiboVm.UserId, DateTime.Now.Ticks);
+                    //上传到oss
+                    var ossSucess = ObjectOperate.UploadImage(buckeyName, file.LocalFileName, imgUrl);
+                    if (ossSucess)
+                    {
+                        files.Add(imgUrl);
+                    }
+                }
+                weiboVm.ContentValue = string.Join(",", files);
+                //数据库操作
+
+                // Send OK Response along with saved file names to the client.  
+                return Request.CreateResponse(HttpStatusCode.OK, new JsonResultViewModel
+                {
+                    IsSuccess = 1,
+                    Description = "ok",
+                    Data = weiboVm
+                });
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+        /// <summary>
+        /// 创建一个 Provider 用于重命名接收到的文件 
+        /// </summary>
+        public class CustomMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
+        {
+            public CustomMultipartFormDataStreamProvider(string path) : base(path) { }
+            public override string GetLocalFileName(HttpContentHeaders headers)
+            {
+                //var fileName = "weiboimage/" + DateTime.Now.ToString("yyyyMMdd") + "/" + headers.ContentDisposition.FileName.Replace("\"", string.Empty);
+                var type =Path.GetExtension(headers.ContentDisposition.FileName.Replace("\"", string.Empty));
+                var sb = new StringBuilder(( DateTime.Now.Ticks.ToString()).Replace("\"", "").Trim().Replace(" ", "_")+type);
+                Array.ForEach(Path.GetInvalidFileNameChars(), invalidChar => sb.Replace(invalidChar, '-'));
+                return sb.ToString();
+
+            }
+        }
+
+        private NewWeiBoViewModel InitWeiBoData(CustomMultipartFormDataStreamProvider provider)
+        {
+            var vm = new NewWeiBoViewModel();
+            var formData = provider.FormData;
+            if (formData.HasKeys())
+            {
+                //从header获取
+                if (formData.AllKeys.Contains("userid")&&!string.IsNullOrEmpty(formData.Get("userid")))
+                {
+                    var aa = formData.Get("userid");
+                    vm.UserId = Int32.Parse(aa);
+                }
+                if (formData.AllKeys.Contains("Description") && !string.IsNullOrEmpty(formData.Get("Description")))
+                {
+                    var aa = formData.Get("Description");
+                    vm.Description = aa;
+                }
+                if (formData.AllKeys.Contains("Longitude") && !string.IsNullOrEmpty(formData.Get("Longitude")))
+                {
+                    var aa = formData.Get("Longitude");
+                    vm.Longitude = decimal.Parse(aa);
+                }
+                if (formData.AllKeys.Contains("Latitude") && !string.IsNullOrEmpty(formData.Get("Latitude")))
+                {
+                    var aa = formData.Get("Latitude");
+                    vm.Lantitude = decimal.Parse(aa);
+                }
+                if (formData.AllKeys.Contains("Height") && !string.IsNullOrEmpty(formData.Get("Height")))
+                {
+                    var aa = formData.Get("Height");
+                    vm.Height = decimal.Parse(aa);
+                }
+                if (formData.AllKeys.Contains("LocationText") && !string.IsNullOrEmpty(formData.Get("LocationText")))
+                {
+                    var aa = formData.Get("LocationText");
+                    vm.LocationText = aa;
+                }
+                if (formData.AllKeys.Contains("HardWareType") && !string.IsNullOrEmpty(formData.Get("HardWareType")))
+                {
+                    var aa = formData.Get("HardWareType");
+                    vm.HardWareType = aa;
+                }
+                vm.IsRePost = false;
+                vm.ContentType = WeiBoContentTypeEnum.Image;
+                vm.State = WeiBoStateEnum.Normal;
+            }
+            return vm;
+        }
+    
     }
 }
