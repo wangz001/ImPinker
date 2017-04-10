86a3794b48185c515dbdb1f9dcd33765ff0a2102
wangz001 <bwlang@sina.cn>
2017-04-10 17:10:44 +0800

    11

---
+++ ImPinker/ImpinkerApi/Controllers/ArticleVoteController.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http;
+using System.Web.Mvc;
+using ImpinkerApi.Filters;
+using ImpinkerApi.Models;
+
+namespace ImpinkerApi.Controllers
+{
+    public class ArticleVoteController : BaseApiController
+    {
+        //
+        // GET: /ArticleVote/
+
+        public HttpResponseMessage Index()
+        {
+
+            return GetJson(new JsonResultViewModel
+            {
+                IsSuccess = 0,
+                Data = null,
+                Description = "没有更多数据"
+            });
+        }
+        #region 文章点赞
+        
+        
+
+        #endregion
+
+        #region 文章评论
+
+        
+        #endregion
+
+        #region 文章收藏
+        /// <summary>
+        /// 用户收藏文章
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [TokenCheck]
+        public HttpResponseMessage NewArticleCollect(int articleId)
+        {
+            return GetJson(new JsonResultViewModel
+            {
+                IsSuccess = 0,
+                Data = null,
+                Description = "没有更多数据"
+            });
+        }
+
+        #endregion
+    }
+}
