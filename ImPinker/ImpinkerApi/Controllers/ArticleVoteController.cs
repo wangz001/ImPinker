c018ea7f16562daa7c465910b4e6b5be098f7a0d
wangz001 <bwlang@sina.cn>
2017-04-11 16:19:56 +0800

    collect&comment

--- ImPinker/ImpinkerApi/Controllers/ArticleVoteController.cs
+++ ImPinker/ImpinkerApi/Controllers/ArticleVoteController.cs
@@ -1,5 +1,10 @@
-﻿using System.Net.Http;
-using System.Web.Mvc;
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using ImBLL;
+using ImModel;
+using ImModel.Enum;
+using ImpinkerApi.Common;
 using ImpinkerApi.Filters;
 using ImpinkerApi.Models;
 
@@ -7,28 +12,55 @@ namespace ImpinkerApi.Controllers
 {
     public class ArticleVoteController : BaseApiController
     {
-        //
-        // GET: /ArticleVote/
+        ArticleCommentBll _articleCommentBll =new ArticleCommentBll();
+        
 
-        public HttpResponseMessage Index()
+        #region 文章评论
+        /// <summary>
+        /// 评论
+        /// </summary>
+        /// <param name="vm"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [TokenCheck]
+        public HttpResponseMessage NewArticleComment([FromBody]NewArticleCommentVm vm)
         {
-
+            if (string.IsNullOrEmpty(vm.CommentStr)||vm.ArticleId==0)
+            {
+                return GetJson(new JsonResultViewModel
+                {
+                    IsSuccess = 0,
+                    Data = null,
+                    Description = "评论内容不能为空"
+                }); 
+            }
+            var userinfo = TokenHelper.GetUserInfoByHeader(Request.Headers);
+            var model = new ArticleComment
+            {
+                ArticleId = vm.ArticleId,
+                Content = vm.CommentStr,
+                UserId = userinfo.Id,
+                ToCommentId = 0,
+                CreateTime = DateTime.Now
+            };
+            var flag = _articleCommentBll.Add(model);
+            if (flag)
+            {
+                return GetJson(new JsonResultViewModel
+                {
+                    IsSuccess = 1,
+                    Data = null,
+                    Description = "评论成功"
+                });
+            }
             return GetJson(new JsonResultViewModel
             {
                 IsSuccess = 0,
                 Data = null,
-                Description = "没有更多数据"
+                Description = "评论失败"
             });
         }
-        #region 文章点赞
-        
-        
 
-        #endregion
-
-        #region 文章评论
-
-        
         #endregion
 
         #region 文章收藏
@@ -38,8 +70,46 @@ namespace ImpinkerApi.Controllers
         /// <returns></returns>
         [HttpGet]
         [TokenCheck]
-        public HttpResponseMessage NewArticleCollect(int articleId)
+        public HttpResponseMessage NewArticleCollect(long articleId)
         {
+            var userinfo = TokenHelper.GetUserInfoByHeader(Request.Headers);
+            var flag = new ArticleCollectionBll().AddCollect(articleId, userinfo.Id);
+            if (flag)
+            {
+                return GetJson(new JsonResultViewModel
+                {
+                    IsSuccess = 1,
+                    Data = null,
+                    Description = "收藏成功"
+                });
+            }
+            return GetJson(new JsonResultViewModel
+            {
+                IsSuccess = 0,
+                Data = null,
+                Description = "没有更多数据"
+            });
+        }
+        /// <summary>
+        /// 取消收藏文章
+        /// </summary>
+        /// <param name="articleId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [TokenCheck]
+        public HttpResponseMessage CancelArticleCollect(long articleId)
+        {
+            var userinfo = TokenHelper.GetUserInfoByHeader(Request.Headers);
+            var flag = new ArticleCollectionBll().RemoveCollect(articleId, userinfo.Id);
+            if (flag)
+            {
+                return GetJson(new JsonResultViewModel
+                {
+                    IsSuccess = 1,
+                    Data = null,
+                    Description = "取消收藏成功"
+                });
+            }
             return GetJson(new JsonResultViewModel
             {
                 IsSuccess = 0,
---
+++ ImPinker/ImpinkerApi/Models/ArticleVoteViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImpinkerApi.Models
+{
+    public class ArticleVoteViewModel
+    {
+    }
+    /// <summary>
+    /// 用户文章评论实体
+    /// </summary>
+    public class NewArticleCommentVm
+    {
+        public long ArticleId { get; set; }
+
+        public string CommentStr { get; set; }
+        /// <summary>
+        /// 不为0表示 是评论某条评论
+        /// </summary>
+        public long ToCommentId { get; set; }
+    }
+}
