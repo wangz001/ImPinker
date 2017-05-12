ad1e18f512b5bb661e68aede6d6f00084f1856d9
wangz001 <bwlang@sina.cn>
2017-05-13 00:40:00 +0800

    newyouji

--- ImpinkerHBuild/MyAutos/js/article/youji_new.js
+++ ImpinkerHBuild/MyAutos/js/article/youji_new.js
@@ -1,7 +1,7 @@
 /**
  * 发帖子，图文排版
  **/
-(function(app, owner) {
+(function(apputil, owner) {
 	owner.showWeiBoItems = function(contentLists, items) {
 		var imgPath = 'http://img.myautos.cn/';
 		for(var i = 0; i < items.length; i++) {
@@ -10,27 +10,100 @@
 				var imgs = item.ContentValue.split(',');
 				var imgHtmlStr = "";
 				for(var j = 0; j < imgs.length; j++) {
-					imgHtmlStr += '<p><img src="' + imgPath + imgs[j] + '"></p>';
-					imgHtmlStr += '<textarea>' + item.Description + '</textarea>';
-					owner.InitDateObject(contentLists,"img",imgPath + imgs[j]);
-					owner.InitDateObject(contentLists,"text",item.Description);
+					imgHtmlStr += '<p name="yjcontent"><img src="' + imgPath + imgs[j] + '"></p>';
+					imgHtmlStr += '<textarea name="yjcontent">' + item.Description + '</textarea>';
+					owner.InitDateObject(contentLists, "img", imgPath + imgs[j]);
+					owner.InitDateObject(contentLists, "text", item.Description);
 				}
 				$(".mui-content-padded").append(imgHtmlStr);
 			}
 			if(item.Description.length > 0) {
-				var textareaStr = '<textarea value="' + item.Description + '"></textarea>'
+				var textareaStr = '<textarea name="yjcontent" value="' + item.Description + '"></textarea>'
 				$(".mui-content-padded").append(textareaStr);
-				owner.InitDateObject(contentLists,"text",item.Description);
+				owner.InitDateObject(contentLists, "text", item.Description);
 			}
 		}
 	}
-	
-	owner.InitDateObject=function(contentLists,dateType,dateValue){
-		var obj=new Object;
-		obj.dateType=dateType;
-		obj.dateValue=dateValue;
+
+	owner.InitDateObject = function(contentLists, dateType, dateValue) {
+		var obj = new Object;
+		obj.dateType = dateType;
+		obj.dateValue = dateValue;
 		contentLists.push(obj);
 	}
-	
+	//根据标签 name属性，拼接出 content
+	owner.getcontenthtml = function(nodename) {
+		var resultStr = "";
+		var yjcontents = document.getElementsByName(nodename);
+		for(var i = 0; i < yjcontents.length; i++) {
+			var htmlnode = yjcontents[i];
+			if($(htmlnode).is("textarea")) {
+				//console.log('是文本2:'+$(htmlnode).val());
+				resultStr += '<p>' + $(htmlnode).val() + '</p>';
+			} else {
+				console.log('aa');
+				resultStr += '<p>' + htmlnode.innerHTML + '</p>'
+			}
+		}
+		return resultStr;
+	}
+	owner.createYouji = function(titleStr, callback) {
+		callback = callback || $.noop;
+		var userstate = app.getState();
+		mui.ajax('http://api.myautos.cn/api/article/NewArticle', {
+			data: {
+				ArticleName: titleStr
+			},
+			headers: {
+				"username": userstate.account,
+				"usertoken": userstate.token
+			},
+			dataType: 'json', //服务器返回json格式数据
+			type: 'post', //HTTP请求类型
+			success: function(data) {
+				console.log(JSON.stringify(data));
+				console.log(data.IsSuccess);
+				if(data.IsSuccess == '1' && data.Data != null) {
+					console.log('2');
+					var articleinfo = data.Data;
+					return callback(articleinfo.Id);
+				} else {
+					return;
+				}
+			},
+			error: function(xhr, type, errorThrown) {
+				console.log(JSON.stringify(errorThrown));
+			}
+		});
+	}
+
+	owner.updateTitle = function(titleStr, callback) {
+		callback = callback || $.noop;
+		var userstate = app.getState();
+		mui.ajax('http://api.myautos.cn/api/article/NewArticle', {
+			data: {
+				ArticleName: titleStr
+			},
+			headers: {
+				"username": userstate.account,
+				"usertoken": userstate.token
+			},
+			dataType: 'json', //服务器返回json格式数据
+			type: 'post', //HTTP请求类型
+			success: function(data) {
+				console.log(JSON.stringify(data));
+				console.log('1');
+				if(data.IsSuccess == 1 && data.Data != null && data.Data.length > 0) {
+					var articleinfo = data.Data;
+					return callback(articleinfo.Id);
+				} else {
+					return;
+				}
+			},
+			error: function(xhr, type, errorThrown) {
+				console.log(JSON.stringify(errorThrown));
+			}
+		});
+	}
 
 }(mui, window.edityoujiUtil = {}));
