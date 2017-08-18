0926099126525180ffdaf82841e49c886cb1797e
wangz001 <bwlang@sina.cn>
2017-08-18 15:15:47 +0800

    redistest

---
+++ ImPinker/Common/Redis/RedisHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Redis
+{
+    public class RedisHelper
+    {
+        private static string _conn = ConfigurationManager.AppSettings["SERedis"] ?? "127.0.0.1:6379";
+
+        //http://www.cnblogs.com/bnbqian/p/4962855.html
+
+    }
+}
