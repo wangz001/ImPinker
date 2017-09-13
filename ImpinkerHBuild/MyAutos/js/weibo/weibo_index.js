9cdbc881fbdc482b957bb29af190b1f8d9da03fe
wangz001 <bwlang@sina.cn>
2017-09-13 22:04:41 +0800

    水中花

--- ImpinkerHBuild/MyAutos/js/weibo/weibo_index.js
+++ ImpinkerHBuild/MyAutos/js/weibo/weibo_index.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-	bindVote();
 	//评论  
 	mui('.mui-scroll').on('tap', '.mui-icon-compose', function() {
 		var weiboid = this.getAttribute("weiboid");
@@ -15,38 +14,28 @@ $(document).ready(function() {
 			}
 		});
 	});
-});
-
-function bindVote() {
 	//点赞
-	var footers = $(".mui-card-footer");
-	for(var i = 0; i < footers.length; i++) {
-		var footer = footers[i];
-		var span = $(footer).find("span:eq(0)");
-		$(span).unbind("click"); //加载多页的时候防止重复绑定
-		$(span).bind("click", function() {
-			var heartType = $(this).attr("class");
-			var count = $(this).text();
-			var weiboid = this.getAttribute("weiboid");
-			console.log(count);
-			if(heartType.indexOf("mui-icon-extra-heart-filled") != -1) {
-//				$(this).removeClass("mui-icon-extra-heart-filled");
-//				$(this).addClass("mui-icon-extra-heart");
-//				if(count > 0) {
-//					$(this).find("em").html(parseInt(count) - 1);
-//				}
-				mui.toast("您已赞过此微博");
-			} else {
-				$(this).removeClass("mui-icon-extra-heart");
-				$(this).addClass("mui-icon-extra-heart-filled");
-				$(this).find("em").html(parseInt(count) + 1);
-				mui.toast("点赞成功！");
-				
-				sendVote(weiboid, false)
-			}
-		});
-	}
-}
+	mui('.mui-scroll').on('tap', '.votebtn', function() {
+		var heartType = $(this).attr("class");
+		var count = $(this).text();
+		var weiboid = this.getAttribute("weiboid");
+		console.log(count);
+		if(heartType.indexOf("mui-icon-extra-heart-filled") != -1) {
+			//				$(this).removeClass("mui-icon-extra-heart-filled");
+			//				$(this).addClass("mui-icon-extra-heart");
+			//				if(count > 0) {
+			//					$(this).find("em").html(parseInt(count) - 1);
+			//				}
+			mui.toast("您已赞过此微博");
+		} else {
+			$(this).removeClass("mui-icon-extra-heart");
+			$(this).addClass("mui-icon-extra-heart-filled");
+			$(this).find("em").html(parseInt(count) + 1);
+			mui.toast("点赞成功！");
+			sendVote(weiboid, false)
+		}
+	});
+});
 
 function sendVote(weiboid, isVote) {
 	var url = "http://api.myautos.cn/api/weibovote/newweibovote";
@@ -54,7 +43,7 @@ function sendVote(weiboid, isVote) {
 		"weiboid": weiboid
 	}
 	commonUtil.sendRequestWithToken(url, data, true, function(data) {
-		if(data.IsSuccess == 1 ) {
+		if(data.IsSuccess == 1) {
 			storageUtil.setWeiboVote(weiboid);
 			console.log("aa");
 		} else {
