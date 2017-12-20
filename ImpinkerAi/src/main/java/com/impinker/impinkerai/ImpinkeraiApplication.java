a8484ea8695876f84ed71c2cce68f7c7a11f8e4e
wangzheng <wangzheng1@bitauto.com>
2017-12-20 17:44:38 +0800

    ai

---
+++ ImpinkerAi/src/main/java/com/impinker/impinkerai/ImpinkeraiApplication.java
@@ -0,0 +1,12 @@
+package com.impinker.impinkerai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpinkeraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpinkeraiApplication.class, args);
+	}
+}
